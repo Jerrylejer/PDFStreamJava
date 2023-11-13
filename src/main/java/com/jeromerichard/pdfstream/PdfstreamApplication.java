f89c7948a372aa9b49c740c0a02aaa28e15408cb
Jerrylejer <jerrylejer23@yahoo.fr>
2023-11-13 17:17:18 +0100

    Initial commit

---
+++ src/main/java/com/jeromerichard/pdfstream/PdfstreamApplication.java
@@ -0,0 +1,13 @@
+package com.jeromerichard.pdfstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfstreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfstreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeromerichard/pdfstream/PdfstreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeromerichard.pdfstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfstreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
