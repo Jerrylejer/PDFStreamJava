9ee8ae6715c245dd4325b7c9f56f1fd16c3b347d
Jerrylejer <jerrylejer23@yahoo.fr>
2024-01-18 10:55:33 +0100

    première entité "Pdf"

--- src/main/java/com/jeromerichard/pdfstream/Entity/Pdf.java
+++ src/main/java/com/jeromerichard/pdfstream/Entity/Pdf.java
@@ -1,4 +1,137 @@
 package com.jeromerichard.pdfstream.Entity;
 
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name="pdf")
 public class Pdf {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name="title")
+    private String title;
+    @Column(name="small_description")
+    private String smallDescription;
+    @Column(name="description")
+    private String description;
+    @Column(name="image")
+    private String image;
+    @Column(name="author")
+    private String author;
+    @Column(name="type")
+    private String type;
+    @Column(name="size")
+    private Integer size;
+    @Column(name="counter")
+    private Integer counter;
+    @Column(name="created_at")
+    private Date createdAt;
+    @Column(name="updated_at")
+    private Date updateAt;
+
+    public Pdf() {
+    }
+
+    public Pdf(String title, String smallDescription, String description, String image, String author, String type, Integer size, Integer counter, Date createdAt, Date updateAt) {
+        this.title = title;
+        this.smallDescription = smallDescription;
+        this.description = description;
+        this.image = image;
+        this.author = author;
+        this.type = type;
+        this.size = size;
+        this.counter = counter;
+        this.createdAt = createdAt;
+        this.updateAt = updateAt;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSmallDescription() {
+        return smallDescription;
+    }
+
+    public void setSmallDescription(String smallDescription) {
+        this.smallDescription = smallDescription;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+
+    public Integer getCounter() {
+        return counter;
+    }
+
+    public void setCounter(Integer counter) {
+        this.counter = counter;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Date updateAt) {
+        this.updateAt = updateAt;
+    }
 }
