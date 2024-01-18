80feffe0ca042526b753d139adbcc2821a440796
Jerrylejer <jerrylejer23@yahoo.fr>
2024-01-18 11:17:12 +0100

    entité "Profile"

---
+++ src/main/java/com/jeromerichard/pdfstream/Entity/Profile.java
@@ -0,0 +1,126 @@
+package com.jeromerichard.pdfstream.Entity;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name="profile")
+public class Profile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name="gender")
+    private String gender;
+    @Column(name="firstname")
+    private String firstname;
+    @Column(name="lastname")
+    private String lastname;
+    @Column(name="adress1")
+    private String adress1;
+    @Column(name="adress2")
+    private String adress2;
+    @Column(name="zipcode")
+    private String zipcode;
+    @Column(name="city")
+    private String city;
+    @Column(name="created_at")
+    private Date createdAt;
+    @Column(name="updated_at")
+    private Date updateAt;
+
+    public Profile() {
+    }
+
+    public Profile(String gender, String firstname, String lastname, String adress1, String adress2, String zipcode, String city, Date createdAt, Date updateAt) {
+        this.gender = gender;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.adress1 = adress1;
+        this.adress2 = adress2;
+        this.zipcode = zipcode;
+        this.city = city;
+        this.createdAt = createdAt;
+        this.updateAt = updateAt;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getAdress1() {
+        return adress1;
+    }
+
+    public void setAdress1(String adress1) {
+        this.adress1 = adress1;
+    }
+
+    public String getAdress2() {
+        return adress2;
+    }
+
+    public void setAdress2(String adress2) {
+        this.adress2 = adress2;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Date updateAt) {
+        this.updateAt = updateAt;
+    }
+}
