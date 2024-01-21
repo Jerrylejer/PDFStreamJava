54a8a16b42de58345cfda0f8649c38af3d6a77bc
Jerrylejer <jerrylejer23@yahoo.fr>
2024-01-21 13:58:13 +0100

    Dépendance ModelMapper ajoutée et instanciation dans le projet ok, voir si la version récente ne cause pas de bugs d'utilisation

--- src/main/java/com/jeromerichard/pdfstream/PdfstreamApplication.java
+++ src/main/java/com/jeromerichard/pdfstream/PdfstreamApplication.java
@@ -1,13 +1,21 @@
 package com.jeromerichard.pdfstream;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PdfstreamApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PdfstreamApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		// Instanciation de ModelMapper pour le projet et son utilisation
+		return new ModelMapper();
 	}
 
 }
