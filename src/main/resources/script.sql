7eb0e8290d546a87506fe17ad8035eae5defd57e
Jerrylejer <jerrylejer23@yahoo.fr>
2024-04-08 23:17:01 +0200

    Modification du script.sql et essai de switch de BDD dans application.properties => Création des tables ok mais insert into ne fonctionne pas => à voir

--- src/main/resources/script.sql
+++ src/main/resources/script.sql
@@ -1,21 +1,21 @@
 -- DROP TABLES
-DROP TABLE IF EXISTS `alert`;
-DROP TABLE IF EXISTS `article`;
-DROP TABLE IF EXISTS `category`;
-DROP TABLE IF EXISTS `collection`;
-DROP TABLE IF EXISTS `donation`;
-DROP TABLE IF EXISTS `evaluation`;
-DROP TABLE IF EXISTS `pdf`;
-DROP TABLE IF EXISTS `pdf_category`;
-DROP TABLE IF EXISTS `pdf_collection`;
-DROP TABLE IF EXISTS `search`;
-DROP TABLE IF EXISTS `user`;
-DROP TABLE IF EXISTS `role`;
-DROP TABLE IF EXISTS `user_role`;
+DROP TABLE IF EXISTS `alert` CASCADE;
+DROP TABLE IF EXISTS `article` CASCADE;
+DROP TABLE IF EXISTS `category` CASCADE;
+DROP TABLE IF EXISTS `collection` CASCADE;
+DROP TABLE IF EXISTS `donation` CASCADE;
+DROP TABLE IF EXISTS `evaluation` CASCADE;
+DROP TABLE IF EXISTS `pdf` CASCADE;
+DROP TABLE IF EXISTS `pdf_category` CASCADE;
+DROP TABLE IF EXISTS `pdf_collection` CASCADE;
+DROP TABLE IF EXISTS `search` CASCADE;
+DROP TABLE IF EXISTS `user` CASCADE;
+DROP TABLE IF EXISTS `role` CASCADE;
+DROP TABLE IF EXISTS `user_role` CASCADE;
 
 -- CREATE TABLES
 CREATE TABLE IF NOT EXISTS `alert` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `title` varchar(50) DEFAULT NULL,
   `description` text,
   `state` varchar(50) DEFAULT NULL,
@@ -24,50 +24,52 @@ CREATE TABLE IF NOT EXISTS `alert` (
   `pdf_id` int DEFAULT NULL,
   `article_id` int DEFAULT NULL,
   `user_id` bigint DEFAULT NULL,
-  PRIMARY KEY (`id`),
+   CONSTRAINT `FK_article` FOREIGN KEY (`article_id`) REFERENCES `article` (`id`),
+   CONSTRAINT `FK_pdf` FOREIGN KEY (`pdf_id`) REFERENCES `pdf` (`id`),
+   CONSTRAINT `FK_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
 );
 
 CREATE TABLE IF NOT EXISTS `article` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `title` varchar(50) DEFAULT NULL,
   `description` text,
   `ordering` int DEFAULT NULL,
   `created_at` datetime(6) DEFAULT NULL,
   `updated_at` datetime(6) DEFAULT NULL,
-  PRIMARY KEY (`id`)
 );
 
 CREATE TABLE IF NOT EXISTS `category` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `title` varchar(50) DEFAULT NULL,
   `parent_id` int DEFAULT NULL,
   `created_at` datetime(6) DEFAULT NULL,
   `updated_at` datetime(6) DEFAULT NULL,
-  PRIMARY KEY (`id`),
+  CONSTRAINT `FK_parent` FOREIGN KEY (`parent_id`) REFERENCES `category` (`id`),
 );
 
 CREATE TABLE IF NOT EXISTS `collection` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `created_at` datetime(6) DEFAULT NULL,
   `updated_at` datetime(6) DEFAULT NULL,
-  `user_id` bigint DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `UK_collection_user` (`user_id`)
+  `user_id` bigint UNIQUE DEFAULT NULL,
+  CONSTRAINT `FK_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
 );
 
 CREATE TABLE IF NOT EXISTS `donation` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `amount` int DEFAULT NULL,
   `message` varchar(255) DEFAULT NULL,
   `created_at` datetime(6) DEFAULT NULL,
   `pdf_id` int DEFAULT NULL,
   `user_beneficiary_id` bigint DEFAULT NULL,
   `user_donor_id` bigint DEFAULT NULL,
-  PRIMARY KEY (`id`),
+   CONSTRAINT `FK_user_beneficiary` FOREIGN KEY (`user_beneficiary_id`) REFERENCES `user` (`id`),
+   CONSTRAINT `FK_user_donor` FOREIGN KEY (`user_donor_id`) REFERENCES `user` (`id`),
+   CONSTRAINT `FK_pdf` FOREIGN KEY (`pdf_id`) REFERENCES `pdf` (`id`),
 );
 
 CREATE TABLE IF NOT EXISTS `evaluation` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `title` varchar(50) DEFAULT NULL,
   `comment` varchar(255) DEFAULT NULL,
   `star` tinyint DEFAULT NULL,
@@ -75,11 +77,12 @@ CREATE TABLE IF NOT EXISTS `evaluation` (
   `updated_at` datetime(6) DEFAULT NULL,
   `pdf_id` int DEFAULT NULL,
   `user_id` bigint DEFAULT NULL,
-  PRIMARY KEY (`id`),
+   CONSTRAINT `FK_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+   CONSTRAINT `FK_pdf` FOREIGN KEY (`pdf_id`) REFERENCES `pdf` (`id`),
 );
 
 CREATE TABLE IF NOT EXISTS `pdf` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `title` varchar(50) DEFAULT NULL,
   `small_description` varchar(255) DEFAULT NULL,
   `description` text,
@@ -91,93 +94,48 @@ CREATE TABLE IF NOT EXISTS `pdf` (
   `created_at` datetime(6) DEFAULT NULL,
   `updated_at` datetime(6) DEFAULT NULL,
   `user_id` bigint DEFAULT NULL,
-  PRIMARY KEY (`id`),
+   CONSTRAINT `FK_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
 );
 
 CREATE TABLE IF NOT EXISTS `pdf_category` (
   `category_id` int NOT NULL,
   `pdf_id` int NOT NULL,
   PRIMARY KEY (`category_id`,`pdf_id`),
+  CONSTRAINT `FK_pdf` FOREIGN KEY (`pdf_id`) REFERENCES `pdf` (`id`),
+  CONSTRAINT `FK_category` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`),
 );
 
 CREATE TABLE IF NOT EXISTS `pdf_collection` (
   `collection_id` int NOT NULL,
   `pdf_id` int NOT NULL,
   PRIMARY KEY (`collection_id`,`pdf_id`),
+  CONSTRAINT `FK_pdf` FOREIGN KEY (`pdf_id`) REFERENCES `pdf` (`id`),
+  CONSTRAINT `FK_collection` FOREIGN KEY (`collection_id`) REFERENCES `collection` (`id`),
 );
 
 CREATE TABLE IF NOT EXISTS `role` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `name` enum('ROLE_ADMIN','ROLE_USER') DEFAULT NULL,
-  PRIMARY KEY (`id`)
-);
-
-CREATE TABLE IF NOT EXISTS `search` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `create_at` datetime(6) DEFAULT NULL,
-  `search_user_id` bigint DEFAULT NULL,
-  `result` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`),
 );
 
 CREATE TABLE IF NOT EXISTS `user` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) DEFAULT NULL,
+  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) UNIQUE DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
+  `email` varchar(255) UNIQUE DEFAULT NULL,
   `avatar` mediumblob,
   `bio` varchar(255) DEFAULT NULL,
   `created_at` datetime(6) DEFAULT NULL,
   `updated_at` datetime(6) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `UK_username` (`username`),
-  UNIQUE KEY `UK_email` (`email`)
 );
 
 CREATE TABLE IF NOT EXISTS `user_role` (
   `role_id` int NOT NULL,
   `user_id` bigint NOT NULL,
   PRIMARY KEY (`role_id`,`user_id`),
+  CONSTRAINT `FK_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `FK_role` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`),
 );
-
--- AJOUT DES FOREIGN KEYS
-ALTER TABLE `alert`
-  ADD CONSTRAINT `FK_alert_article` FOREIGN KEY (`article_id`) REFERENCES `article` (`id`),
-  ADD CONSTRAINT `FK_alert_pdf` FOREIGN KEY (`pdf_id`) REFERENCES `pdf` (`id`),
-  ADD CONSTRAINT `FK_alert_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
-
-ALTER TABLE `category`
-  ADD CONSTRAINT `FK_category_parent` FOREIGN KEY (`parent_id`) REFERENCES `category` (`id`);
-
-ALTER TABLE `collection`
-  ADD CONSTRAINT `FK_collection_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
-
-ALTER TABLE `donation`
-  ADD CONSTRAINT `FK_donation_user_beneficiary` FOREIGN KEY (`user_beneficiary_id`) REFERENCES `user` (`id`),
-  ADD CONSTRAINT `FK_donation_user_donor` FOREIGN KEY (`user_donor_id`) REFERENCES `user` (`id`),
-  ADD CONSTRAINT `FK_donation_pdf` FOREIGN KEY (`pdf_id`) REFERENCES `pdf` (`id`);
-
-ALTER TABLE `evaluation`
-  ADD CONSTRAINT `FK_evaluation_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
-  ADD CONSTRAINT `FK_evaluation_pdf` FOREIGN KEY (`pdf_id`) REFERENCES `pdf` (`id`);
-
-ALTER TABLE `pdf`
-  ADD CONSTRAINT `FK_pdf_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
-
-ALTER TABLE `pdf_category`
-  ADD CONSTRAINT `FK_pdf_category_pdf` FOREIGN KEY (`pdf_id`) REFERENCES `pdf` (`id`),
-  ADD CONSTRAINT `FK_pdf_category_category` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`);
-
-ALTER TABLE `pdf_collection`
-  ADD CONSTRAINT `FK_pdf_collection_pdf` FOREIGN KEY (`pdf_id`) REFERENCES `pdf` (`id`),
-  ADD CONSTRAINT `FK_pdf_collection_collection` FOREIGN KEY (`collection_id`) REFERENCES `collection` (`id`);
-
-ALTER TABLE `search`
-  ADD CONSTRAINT `FK_search_user` FOREIGN KEY (`search_user_id`) REFERENCES `user` (`id`);
-
-ALTER TABLE `user_role`
-  ADD CONSTRAINT `FK_user_role_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
-  ADD CONSTRAINT `FK_user_role_role` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`);
 
  -- INSERT DES DATAS -> ARTICLE
  INSERT INTO `article` (`title`, `description`, `ordering`, `created_at`, `updated_at`) VALUES
@@ -250,7 +208,7 @@ INSERT INTO `category` (`title`, `parent_id`, `created_at`, `updated_at`) VALUES
 ('Arts plastiques', 18, NOW(), NULL),
 ('Arts plastiques', 19, NOW(), NULL);
 
--- Datas à insérer pour le type de rôle
+ -- INSERT DES DATAS -> ROLE
 INSERT INTO `role` (`name`) VALUES
 ('ROLE_USER'),
 ('ROLE_ADMIN');
